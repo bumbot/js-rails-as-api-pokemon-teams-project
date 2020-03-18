8e52956dc3b1fb8da5db1b9c22d168746f9eb3bc
github username <github email address>
2020-03-18 11:23:26 -0400

    serializers

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,18 @@
+class TrainersController < ApplicationController
+    def index 
+        trainers = Trainer.all
+        options =
+            {
+                include: [:pokemons]
+            }
+    # options[:include] = [:trainers]
+        render json: TrainerSerializer.new(trainers, options)
+    # render json: trainers.to_json(:include => {
+    #   :pokemons =>{
+    #     :except =>
+    #       [:updated_at, :created_at]
+    #   }
+    #   }, :except => [:updated_at, :created_at]
+    #   )
+    end
+end
